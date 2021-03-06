81290dcfc21094414544d03ee50772b4821a7524
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-03-06 15:41:12 -0500

    Update Javascript to print comments from comment array.

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,109 @@
 let formCapture = document.querySelector(".comments__form");
+const date = new Date();
+// console.dir(formCapture);
 
-console.dir(formCapture);
 
-let commentArray = [];
-const answer = [];
-const commentSlot =  document.querySelector;
 
+let commentArray = [
+    {
+        name: "Theodore Duncan",
+        date: "11/15/2018",
+        comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever"
+    },
+    {
+        name: "Gary Wong",
+        date: "12/12/2018",
+        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    },
+    {
+        name: "Micheal Lyons",
+        date: "12/18/2018",
+        comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+    },
+];
+
+// function displayComments(arr) {
+//     let commentsAll = '';
+//     arr.forEach((comment) => {
+//         let comments = `<div class="comment__image-container">
+//         <div class="comment__image comment__profile-pic"></div>
+//         </div> 
+//         <div class="comment__top">
+//         <div class="comment__name-date-cont">
+//         <p class="comment__name">${comment.name}</p>
+//         <time class="comment__date" datetime="2018-12-18">${comment.date}</time>
+//         </div>    
+//           <p class="comment__content"
+//           >${comment.comment}</p></div>`;
+//           commentsAll = commentsAll + comments;
+//     })
+// }
+
+const commentSection = document.querySelector('.comments__container');
+let allComments = '';
+// const answer = [];
+// const commentSlot =  document.querySelector;
+const displayComment = (commentObject) =>{
+    let commentHTML = `<div class="comment"><div class="comment__image-container">
+    <div class="comment__image comment__profile-pic"></div>
+    </div> 
+    <div class="comment__top">
+    <div class="comment__name-date-cont">
+    <p class="comment__name">${commentObject.name}</p>
+    <time class="comment__date" datetime="2018-12-18">${commentObject.date}</time>
+    </div>    
+    <p class="comment__content"
+    >${commentObject.comment}</p></div></div>`;
+    allComments = allComments + commentHTML;
+    commentArray.push(commentObject);
+    commentSection.innerHTML = allComments;
+    return allComments;
+}
+
+
+
+
+// const date = new Date();
 formCapture.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = e.target.name.value;
-    const comment = e.target.commentField.value;
-    // console.log(name);
-    // console.log(comment);
-    answer.push(name);
-    answer.push(comment);
-    const pusher = document.createElement("div");
-    pusher.className = "comment"
-    pusher.innerHTML = submitForm(answer);
-    console.log(pusher);
-    const commentSection = document.querySelector(".comments__container");
-    commentSection.append(pusher);
+    const commentText = e.target.commentField.value;
+    const commentDate = date.toLocaleDateString();
+    let newCommentObject = {
+        name: name,
+        date: commentDate,
+        comment: commentText,
+    }
+    commentArray.push(newCommentObject);
+    commentSection.innerHTML = commentArray.forEach(displayComment);
+    console.log(commentArray);
 })
 
-const date = new Date();
-console.log(date);
-console.log(date.toLocaleDateString());
+commentArray.forEach(displayComment);
 
 
-const submitForm = (array) => {
-return `
-<div class="comment__image-container">
-<div class="comment__image comment__profile-pic"></div>
-</div> 
-<div class="comment__top">
-<div class="comment__name-date-cont">
-<p class="comment__name">${array[0]}</p>
-<time class="comment__date" datetime="2018-12-18">${date.toLocaleDateString()}</time>
-</div>    
-  <p class="comment__content"
-  >${array[1]}</p></div>`
-}
 
-console.log(submitForm(answer));
+// console.log(displayComment(commentArray[0]));
 
-console.log(new Date());
+
+
+// const submitForm = (array) => {
+// return `
+// <div class="comment__image-container">
+// <div class="comment__image comment__profile-pic"></div>
+// </div> 
+// <div class="comment__top">
+// <div class="comment__name-date-cont">
+// <p class="comment__name">${array[0]}</p>
+// <time class="comment__date" datetime="${date.toISOString()}">${date.toLocaleDateString()}</time>
+// </div>    
+//   <p class="comment__content"
+//   >${array[1]}</p></div>`
+// }
+
+// console.log(submitForm(answer));
+
+
 
 
 
