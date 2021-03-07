3938fdf0239ec401042ca254fa13237cb63e7222
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-03-07 17:59:12 -0500

    Cleaned up index.js and added explanatory comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,49 @@
+// GLOBAL VARIABLE DECLARATIONS
+
+//Variable formCapture declared to capture form submissions line 64
 let formCapture = document.querySelector(".comments__form");
+
+
+//Variable date declared in order to add accurate Date in comments and and accurate datetime attributes
 const date = new Date();
+
+
+//Variable commentSection declared to identify where to insert HTML into page so comments will display line 63
 const commentSection = document.querySelector('.comments__container');
+
+
+//Variable allComments declared to store HTML which will be added to the page.  Comment HTML is added to allComments by invoking the displayComment function on a comment object.  line 66
 let allComments = '';
-let commentImage = document.querySelector(".comment__profile-pic");
 
 
 
 
+// The comment array is an array of objects that each contain the values that the displayComment function can use to render the comment correctly on the page.
 let commentArray = [
     {
         name: "Theodore Duncan",
         date: "11/15/2018",
+        datetime: "2018-11-15",
         comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever",
         commentImage: ""
     },
     {
         name: "Gary Wong",
         date: "12/12/2018",
+        datetime: "2018-12-12",
         comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
         commentImage: ""
     },
     {
         name: "Micheal Lyons",
         date: "12/18/2018",
+        datetime: "2018-12-18",
         comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
         commentImage: ""
     },
 ];
 
-
+// displayComment function takes a comment object, formats it into HTML, adds it to the allComments variable, and puts the final HTML containing all comments onto the page
 
 const displayComment = (commentObject) => {
 let commentHTML = 
@@ -41,7 +56,7 @@ let commentHTML =
             <div class="comment__name-date-cont">
                 <p class="comment__name">${commentObject.name}
                 </p>
-                <time class="comment__date" datetime="2018-12-18">${commentObject.date}
+                <time class="comment__date" datetime="${commentObject.datetime}">${commentObject.date}
                 </time>
             </div> 
             <p class="comment__content">${commentObject.comment}
@@ -50,9 +65,9 @@ let commentHTML =
     </div>`;
 allComments = allComments + commentHTML;
 commentSection.innerHTML = allComments;
-return allComments;
 }
 
+// This event listener listens for comment section submissions, puts the information from the form into a comment Object, pushes the comment object to the comments Array, invokes the displayComment function on the new comment object to modify it into appropriate HTML and add it to allComments
 
 formCapture.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -60,14 +75,17 @@ formCapture.addEventListener('submit', (e) => {
     const name = e.target.name.value;
     const commentText = e.target.commentField.value;
     const commentDate = date.toLocaleDateString();
+    const isoDate = date.toISOString();
     let backgroundImage = document.querySelector(".profile-pic");
     let answer = ""
+    console.log(isoDate);
     if(backgroundImage.id) {
-        answer = backgroundImage.id
+        answer = backgroundImage.id;
     }
     let newCommentObject = {
         name: name,
         date: commentDate,
+        datetime: isoDate,
         comment: commentText,
         commentImage: answer,
     }
@@ -76,5 +94,6 @@ formCapture.addEventListener('submit', (e) => {
     formCapture.reset();
 })
 
+// Invokes displayComment on each element of the Comments Array.  This loads the stored comments when the page loads.
 commentArray.forEach(displayComment);
 
