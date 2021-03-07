a0b4a523d9aae7c34f1706a7bc4e8dafd65269c2
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-03-07 18:44:37 -0500

    Add functionality for adding new shows to table in javascript.  Cleaned up shows.js and added comments

--- scripts/shows.js
+++ scripts/shows.js
@@ -1,18 +1,6 @@
-console.log("hi0");
+//  No global variables
 
-// const show = [
-//     {
-
-//     }
-// ]
-
-{/* <tr class="show__details">
-<td class="show__date">Mon Dev 17 2018</td>
-<td class="show__label">Ronald Lane</td>
-<td class="show__label">San Fransisco, CA</td>
-<td class="show__label align-right"><button class="show__button">BUY TICKETS</button></td>
-</tr> */}
-
+//  The showsArray is an array of objects that is used to populate the shows table.  It is used as an argument in the displayShowTable function to populate the shows table line 45.  The addShow function can be used to add new show Objects to this array, and therefore more easily update the shows table with new shows line 81.
 const showsArray = [
     {
         showDate: "Mon Dec 17 2018",
@@ -51,24 +39,53 @@ const showsArray = [
     },
 ]
 
+
+//  displayShowTable takes an array as an argument (specifically the array of show objects).  It takes uses the values of the different objects, formats them into HTML that can be inserted into the table of shows, concatenates them all and inserts them into the page.
+
 function displayShowTable(arr) {
     let showTableHTML = '';
     arr.forEach((show) => {
-        let showTableRow = `<tr class="show__details">
-        <td class="show__header--mobile">DATE</td>
-        <td class="show__date">${show.showDate}</td>
-        <td class="show__header--mobile">VENUE</td>
-        <td class="show__label">${show.showVenue}</td>
-        <td class="show__header--mobile">LOCATION</td>
-        <td class="show__label">${show.showCity}</td>
-        <td class="show__label align-right"><button class="show__button">BUY TICKETS</button></td>
-    </tr>`;
+        let showTableRow = `
+        <tr class="show__details">
+            <td class="show__header--mobile">DATE
+            </td>
+            <td class="show__date">${show.showDate}
+            </td>
+            <td class="show__header--mobile">VENUE</td>
+            <td class="show__label">${show.showVenue}
+            </td>
+            <td class="show__header--mobile">LOCATION
+            </td>
+            <td class="show__label">${show.showCity}
+            </td>
+            <td class="show__label align-right">
+                <button class="show__button">BUY TICKETS
+                </button>
+            </td>
+        </tr>`;
     showTableHTML = showTableHTML + showTableRow
     })
-    // console.log(showTableHTML);
     const tbody = document.querySelector('.tbody');
     tbody.innerHTML = showTableHTML;
-    console.log(tbody);
 }
 
+//  Calling the displayShowTable function in order to populate the shows table with shows on page load.
 displayShowTable(showsArray);
+
+
+//  Constructor function used in addShow to add new show objects to the showsArray.
+function Show(date, venue, city){
+    this.showDate = date;
+    this.showVenue = venue;
+    this.showCity = city;
+}
+
+
+//  addShow function takes a date, venue and city as a parameter.  It then uses the Show constructor to use those values and create a new show Object, that it then pushes to the shows array.  Finally, it calls the displayShowTable on the updated showsArray to add the show to the page.  Simply invoke addShow with the appropriate parameters to add a new show to the table.
+function addShow(date, venue, city){
+    showsArray.push(new Show(date, venue, city));
+    displayShowTable(showsArray);
+}
+
+
+
